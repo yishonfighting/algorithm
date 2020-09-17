c65a45d2465ff7714addefb85290a33e7152fbdc
YISHON <yishon@YISHONdeMacBook-Pro.local>
2020-09-17 22:58:17 +0800

    sort list [dfs]

---
+++ combineSortList.go
@@ -0,0 +1,37 @@
+package main
+
+//
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+//
+//
+//示例：
+//
+//输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val > l2.Val {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	} else {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+
+}
